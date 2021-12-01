7ba9962adb69e2825e8aa74735efca04fbeb5af5
aa <aa>
2021-11-30 20:37:03 -0800

    fix docker run script
    fixed example talker/listen cross host failure.

--- src/apollo/docker/scripts/run_docker_cuda10.sh
+++ src/apollo/docker/scripts/run_docker_cuda10.sh
@@ -4,5 +4,7 @@ xhost +local:
 
 docker run --gpus all --privileged -it -e NVIDIA_DRIVER_CAPABILITIES=video,compute,graphics,utility \
     --env="DISPLAY" -env="QT_X11_NO_MITSHM=1" --volume="$HOME/.Xauthority:/root/.Xauthority:rw" \
-    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw"  -v /home/dev:/mnt \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --net=host --ipc=host --shm-size="2G" \
+    -v /home/dev:/mnt \
     apollo/standalone:dev-x86_64-18.04-cuda10.0-v0.0 bash
